72e2a8b0f0215cf469ebec1a57c28411113c85a2
jimmymetts <jimmymetts@bellsouth.net>
2020-04-27 19:11:02 -0500

    added search button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css';
 
 const App = () => {
 
-const APP_ID = '0792501f'
+const APP_ID = '0792501f';
+const APP_KEY = 'fdf070695e8df1f0fe225dd600e33d00';
 
-  return(
+const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button
+        "type="submit">Search</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
