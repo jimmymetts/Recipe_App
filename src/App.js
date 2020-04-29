7995f534ca649dfe7af4eb25fe20c68935306bc5
jimmymetts <jimmymetts@bellsouth.net>
2020-04-29 17:08:50 -0500

    added const getSearch with e.preventDefault

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const [query, setQuery] = useState('chicken');
 
 useEffect( () => {
   getRecipes();
-}, []);  
+}, [getRecipes]);  
  
 const getRecipes = async () => {
   const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
@@ -28,9 +28,12 @@ const getRecipes = async () => {
 const updateSearch = e => {
   setSearch(e.target.value); 
   console.log(search);
+};
+
+const getSearch = e => {
+  e.preventDefault();
 }
 
-const 
   return (
     <div className="App">
       <form className="search-form">
