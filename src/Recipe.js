443eb562ef618650739b042011aca70d607244aa
jimmymetts <jimmymetts@bellsouth.net>
2020-05-02 07:58:29 -0500

    add fat to recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './recipe.module.css';
 
 
-const Recipe = ({title, calories, image, ingredients}) => {
+const Recipe = ({title, calories, image, ingredients, fat}) => {
     return(
         <div className={style.recipe}>
             <h1>{title}</h1>
@@ -13,6 +13,7 @@ const Recipe = ({title, calories, image, ingredients}) => {
             </ol>
             <p>{calories}</p>
             <img className={style.image} src={image} alt=""/>
+            <p>{fat}</p>
         
 
         </div>
