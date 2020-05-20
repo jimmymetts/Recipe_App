736198c0acfa076c9e659ca390cf38d750e441b9
jimmymetts <jimmymetts@bellsouth.net>
2020-05-20 12:25:06 -0500

    update cards

--- src/Recipe.js
+++ src/Recipe.js
@@ -6,16 +6,14 @@ const Recipe = ({title, calories, image, ingredients, fat}) => {
     return(
         <div className={style.recipe}>
             <h1>{title}</h1>
+            <img className={style.image} src={image} alt=""/>
             <ol>
                  {ingredients.map(ingredient => (
                      <li>{ingredient.text}</li>
                  ))}
             </ol>
-            <p>{calories}</p>
-            <img className={style.image} src={image} alt=""/>
-
-        
-
+            <p>Calories: {Math.round(calories)}</p>
+            
         </div>
     );
 }
